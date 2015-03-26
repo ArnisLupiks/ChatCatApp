dbb7f63fd29ed4acae6a3473fa7308e07bd7480d
Arnis <arnislupiks@gmail.com>
2015-03-26 23:42:09 +0000

    some update
    some update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -85,4 +85,71 @@ angular.module('starter.controllers', [])
     store.remove('refreshToken');
     $state.go('login', {}, {reload: true});
   };
+})
+.controller('AppCtrl', function($scope, auth, $cordovaPush, $cordovaDialogs, $cordovaMedia, $cordovaToast, ionPlatform, $http) {
+    $scope.notifications = [];
+    // call to register automatically upon device ready
+    ionPlatform.ready.then(function (device) {
+      $scope.register();
+    });
+    // Register
+    $scope.register = function () {
+    var config = null;
+    if (ionic.Platform.isAndroid()) {
+      config = {
+        "senderID": "828857265859" // REPLACE THIS WITH YOURS FROM GCM CONSOLE - also in the project URL like: https://console.developers.google.com/project/434205989073
+      };
+    }
+
+    $cordovaPush.register(config).then(function (result) {
+      console.log("Register success " + result);
+      $cordovaToast.showShortCenter('Registered for push notifications');
+      $scope.registerDisabled=true;
+      // ** NOTE: Android regid result comes back in the pushNotificationReceived, only iOS returned here
+
+    }, function (err) {
+      console.log("Register error " + err)
+    });
+}
+    // Notification Received
+    $scope.$on('$cordovaPush:notificationReceived', function (event, notification) {
+      console.log(JSON.stringify([notification]));
+      if (ionic.Platform.isAndroid()) {
+      handleAndroid(notification);
+      }
+    });
+      // Android Notification Received Handler
+      function handleAndroid(notification) {
+      // ** NOTE: ** You could add code for when app is in foreground or not, or coming from coldstart here too
+      // via the console fields as shown.
+      console.log("In foreground " + notification.foreground + " Coldstart " + notification.coldstart);
+      if (notification.event == "registered") {
+        $scope.regId = notification.regid;
+        storeDeviceToken("android");
+      }
+      else if (notification.event == "message") {
+        $cordovaDialogs.alert(notification.message, "Push Notification Received");
+        $scope.$apply(function () {
+          $scope.notifications.push(JSON.stringify(notification.message));
+      })
+      }
+      else if (notification.event == "error")
+        $cordovaDialogs.alert(notification.msg, "Push notification error event");
+      else $cordovaDialogs.alert(notification.event, "Push notification handler - Unprocessed Event");
+      }
+    // type: Platform type (ios, android etc)
+    function storeDeviceToken(type) {
+      // Create a random userid to store with it
+      var user = { user: 'user' + Math.floor((Math.random() * 10000000) + 1), type: type, token: $scope.regId };
+        console.log("Post token for registered device with data " + JSON.stringify(user));
+        $http.post('http://46.101.46.12/ChatCat/api/addUserGSM.php', JSON.stringify(user))
+        .success(function (data, status) {
+        console.log("Token stored, device is successfully subscribed to receive push notifications.");
+        })
+        .error(function (data, status) {
+        console.log("Error storing device token." + data + " " + status)
+      }
+      );
+    }
+
 });

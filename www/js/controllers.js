8cf83d8298a20081e613b7f68860c94039080e35
Arnis <arnislupiks@gmail.com>
2015-03-26 12:51:33 +0000

    saves user to database
    saves user to database

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers', [])
-.controller('LoginCtrl', function($scope, auth, $state, store) {
+.controller('LoginCtrl', function($scope, auth, $state, store, $http) {
   function doAuth() {
     auth.signin({
       closable: false,
@@ -8,11 +8,31 @@ angular.module('starter.controllers', [])
       authParams: {
         scope: 'openid offline_access'
       }
-    }, function(profile, idToken, accessToken, state, refreshToken) {
+    }, function(profile, idToken, accessToken, state, $h, refreshToken) {
       store.set('profile', profile);
       store.set('token', idToken);
       store.set('refreshToken', refreshToken);
       $state.go('tab.dash');
+
+      var formData = {
+            uid: profile.user_id,
+            name: profile.name,
+           picture: profile.picture
+          };
+          console.log("a here",formData);
+          $scope.method = 'POST';
+          $scope.url = 'http://46.101.46.12/ChatCat/api/addUser.php';
+          //execute method
+          $http({method: $scope.method, url: $scope.url, data: JSON.stringify(formData), headers : {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}}).
+            success(function(data, status) {
+              //if added information, it will log OK and redirect.
+              console.log("OK", data);
+              //redirect to main post page
+          }).error(function(data, status) {
+              $scope.status = status;
+              console.log("oh, something whent wrong: " , data);
+          });
+
     }, function(error) {
       console.log("There was an error logging in", error);
     });
@@ -46,7 +66,7 @@ angular.module('starter.controllers', [])
 
 .controller('ChatsCtrl', function($scope, Chats, auth) {
   $scope.auth = auth;
-  
+
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
